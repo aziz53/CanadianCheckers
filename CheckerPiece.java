3187a33e633859a1c2d5901f6e2e77e773b2e4c0
yujerry24 <yujerry24@gmail.com>
2018-05-11 11:14:18 -0400

    Outline of checker piece object

---
+++ CheckerPiece.java
@@ -0,0 +1,34 @@
+/**
+ * Auto Generated Java Class.
+ */
+public class CheckerPiece {
+  
+  private int xCoord;
+  private int yCoord;
+  
+  booleanStatus;
+  
+  public CheckerPiece() {
+    this.xCoord=0;
+    this.yCoord=0;
+  }
+  
+  public CheckerPiece(int startingX, int startingY) {
+    this.xCoord=startingX;
+    this.yCoord=startingY;
+  }
+  
+  public int returnXCoord() {
+   return this.xCoord; 
+  }
+  
+  public int returnYCoord() {
+   return this.yCoord; 
+  }
+  
+  public void setCoords(int x, int y) {
+   this.xCoord=x;
+   this.yCoord=y;
+  }
+  
+}
